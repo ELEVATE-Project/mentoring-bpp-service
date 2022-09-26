9899b617d10fc943d7b12ee3fc0117192f30c33a
joffinjoy <joffinjoy@gmail.com>
2022-09-26 09:04:11 +0530

    Trivial Push

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 
+
 app.use(bodyParser.urlencoded({ extended: true, limit: '50MB' }))
 app.use(bodyParser.json({ limit: '50MB' }))
 app.use(cors())
