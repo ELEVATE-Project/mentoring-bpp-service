ba41dafb250f583d86627f4bf4f2d660dd8bb9a2
joffinjoy <joffinjoy@gmail.com>
2022-10-03 15:34:51 +0530

    Added Cancel Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const bpp = require('@controllers/')
 router.post('/search', bpp.search)
 router.post('/init', bpp.init)
 router.post('/confirm', bpp.confirm)
+router.post('/cancel', bpp.cancel)
 
 module.exports = router
