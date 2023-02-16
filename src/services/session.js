8b0631f37402ba903ba25a6a556503f426580f58
Nevil <nevil@tunerlabs.com>
2023-02-17 01:09:47 +0530

    optimized the code

--- src/services/session.js
+++ src/services/session.js
@@ -10,35 +10,38 @@ exports.session = async (requestBody) => {
 	try {
 		const sessionId = requestBody.sessionId
 		const sessionAttendance = await sessionAttendanceQueries.findBySessionId(sessionId)
-		if (!sessionAttendance) return console.log('SessionAttendance Not Found')
-		const userIds = sessionAttendance.map((sessionAttendee) => {
-			return sessionAttendee.userId
-		})
+		if (!sessionAttendance) {
+			return console.log('SessionAttendance Not Found')
+		}
+		const userIds = sessionAttendance.map((sessionAttendee) => sessionAttendee.userId)
 		const users = await userQueries.findByIds(userIds)
-		if (!users) return console.log('Users Not Found')
-
-		const bapIds = users.map((user) => {
-			return user.bapId
-		})
+		if (!users) {
+			return console.log('Users Not Found')
+		}
+		const bapIds = users.map((user) => user.bapId)
 		const baps = await bapQueries.findByIds(bapIds)
-		if (!baps) return console.log('BAP Not Found')
-
-		let usersWithBapAndAttendance = users.map((user) => ({
-			...user,
-			bapInfo: baps.find((f) => f._id.toString() == user.bapId.toString()),
-			sessionAttendance: sessionAttendance.find((f) => f.userId.toString() == user._id.toString()),
-		}))
-
-		usersWithBapAndAttendance.map(async function (user) {
+		if (!baps) {
+			return console.log('BAP Not Found')
+		}
+		let usersWithBapAndAttendance = users.map((user) => {
+			const bapInfo = baps.find((f) => f._id.toString() === user.bapId.toString())
+			const sessionAttendanceInfo = sessionAttendance.find((f) => f.userId.toString() === user._id.toString())
+			return {
+				...user,
+				bapInfo,
+				sessionAttendanceInfo,
+			}
+		})
+		usersWithBapAndAttendance.map(async (user) => {
 			await protocolCallbacks.onUpdate({
 				transactionId: crypto.randomUUID(),
 				messageId: crypto.randomUUID(),
 				bapId: user.bapInfo.bapId,
 				bapUri: user.bapInfo.bapUri,
-				status: user.sessionAttendance.statusText,
+				status: user.sessionAttendanceInfo.statusText,
 				sessionId: sessionId.toString(),
-				fulfillmentId: user.sessionAttendance.fulfillmentId.toString(),
-				orderId: user.sessionAttendance.orderId.toString(),
+				fulfillmentId: user.sessionAttendanceInfo.fulfillmentId.toString(),
+				orderId: user.sessionAttendanceInfo.orderId.toString(),
 			})
 		})
 	} catch (err) {
